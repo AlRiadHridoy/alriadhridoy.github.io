f5d4589a2213875030c62988a5304b7b8e767969
Al Riad Hridoy <64416616+AlRiadHridoy@users.noreply.github.com>
2022-12-16 14:49:35 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
+const btn = document.querySelectorAll("#toggle_theme");
+const theme = document.querySelector("html");
 
-  const btn = document.querySelector("#toggle_theme");
-  const theme = document.querySelector("html");
-  
-  btn.addEventListener("click", () => {
-    theme.classList.toggle("dark")
-  });
-
-  // menu
+[...btn].map((el) =>
+  el.addEventListener("click", () => {
+    theme.classList.toggle("dark");
+  })
+);
 
 const menu = document.querySelector(".menu_btn");
 const menuHeight = document.querySelector(".menu_height");
 const menuHide = document.querySelector(".menu_hide");
-  
+
 menu.addEventListener("click", () => {
-    menuHeight.classList.toggle("h-screen")
-    menuHide.classList.toggle("flex")
-    menuHide.classList.toggle("hidden")
-  })
+  menuHeight.classList.toggle("h-screen");
+  menuHide.classList.toggle("flex");
+  menuHide.classList.toggle("hidden");
+});
+
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+})
+
+// projects
+
+const projects = document.querySelectorAll(".single_project");
+
+
+
+[...projects].map((project) => {
+  project.addEventListener("click", () => {
+    console.log("clicked");
+  });
+})

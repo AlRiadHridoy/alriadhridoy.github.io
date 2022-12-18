ecafa44ce0d689901c09a2fd8322602ca7b879d2
Al Riad Hridoy <64416616+AlRiadHridoy@users.noreply.github.com>
2022-12-18 16:18:36 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,47 +1,71 @@
+// Smooth scrolling
+const navLinks = [...document.querySelectorAll(".nav_links a")];
+const body = document.querySelector("body");
+const html = document.querySelector("html");
+
+function hideMenu() {
+  menuHide.classList.toggle("flex");
+  menuHide.classList.toggle("hidden");
+}
+
+navLinks.map((link) => {
+  link.addEventListener("click", function (e) {
+    hideMenu()
+    lineToggle();
+    e.preventDefault();
+    let section = document.querySelector(this.hash);
+    let navHeight = document.querySelector("nav").clientHeight;
+    window.scrollTo(0, section.offsetTop - navHeight);
+  });
+});
+
+// Dark Mode
 const btn = document.querySelectorAll("#toggle_theme");
 const theme = document.querySelector("html");
-
 [...btn].map((el) =>
   el.addEventListener("click", () => {
     theme.classList.toggle("dark");
   })
 );
+const lineAnime = ["line_anime_1", "line_anime_2", "line_anime_3"];
+const lines = [...document.querySelectorAll(".line")];
+
+function lineToggle() {
+  lines.map((line, index) => {
+    line.classList.toggle(lineAnime[index]);
+  });
+}
 
 const menu = document.querySelector(".menu_btn");
-const menuHeight = document.querySelector(".menu_height");
 const menuHide = document.querySelector(".menu_hide");
-
 menu.addEventListener("click", () => {
-  menuHeight.classList.toggle("h-screen");
-  menuHide.classList.toggle("flex");
-  menuHide.classList.toggle("hidden");
+  hideMenu()
+  lineToggle();
 });
 
-
-const form = document.querySelector("form")
-
+// Form
+const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
-})
+  e.preventDefault();
+});
 
 // projects
-const body = document.querySelector("body");
 const preview = [...document.querySelectorAll(".preview")];
 const previewClose = [...document.querySelectorAll(".preview_close")];
 const projects = [...document.querySelectorAll(".single_project")];
-
 
 projects.map((project, index) => {
   project.addEventListener("click", () => {
     body.classList.add("overflow-hidden");
     preview[index].classList.remove("hidden");
   });
-})
+});
 previewClose.map((item, index) => {
   item.addEventListener("click", () => {
     body.classList.remove("overflow-hidden");
     preview[index].classList.add("hidden");
   });
 });
+
 
 

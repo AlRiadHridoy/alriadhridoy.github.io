da8b745f6a197ca7b448c8bfcbd1a2f58078d6f7
Al Riad Hridoy <64416616+AlRiadHridoy@users.noreply.github.com>
2022-12-20 17:47:42 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,20 +8,20 @@ function hideMenu() {
   menuHide.classList.toggle("hidden");
 }
 
-let navLinkHighlight = 0
+let navLinkHighlight = 0;
 
 navLinks.map((link) => {
   link.addEventListener("click", function (e) {
     // Add border
     if (navLinkHighlight) {
-      navLinkHighlight.classList.remove("border-indigo-700")
-      navLinkHighlight.classList.add("border-transparent")
-    };
-    link.classList.remove("border-transparent")
+      navLinkHighlight.classList.remove("border-indigo-700");
+      navLinkHighlight.classList.add("border-transparent");
+    }
+    link.classList.remove("border-transparent");
     link.classList.add("border-indigo-600");
-    navLinkHighlight = link
+    navLinkHighlight = link;
 
-    hideMenu()
+    hideMenu();
     lineToggle();
     e.preventDefault();
     let section = document.querySelector(this.hash);
@@ -30,14 +30,6 @@ navLinks.map((link) => {
   });
 });
 
-// Dark Mode
-const btn = document.querySelectorAll("#toggle_theme");
-const theme = document.querySelector("html");
-[...btn].map((el) =>
-  el.addEventListener("click", () => {
-    theme.classList.toggle("dark");
-  })
-);
 const lineAnime = ["line_anime_1", "line_anime_2", "line_anime_3"];
 const lines = [...document.querySelectorAll(".line")];
 
@@ -50,7 +42,7 @@ function lineToggle() {
 const menu = document.querySelector(".menu_btn");
 const menuHide = document.querySelector(".menu_hide");
 menu.addEventListener("click", () => {
-  hideMenu()
+  hideMenu();
   lineToggle();
 });
 
@@ -78,9 +70,34 @@ previewClose.map((item, index) => {
   });
 });
 
-
-
 // Contact
 document.querySelector(".form_btn").addEventListener("click", () => {
-  document.querySelector(".onSubmit").classList.remove("hidden")
+  document.querySelector(".onSubmit").classList.remove("hidden");
 });
+
+// Store theme to local storage
+// Dark Mode
+const btn = document.querySelectorAll("#toggle_theme");
+const theme = document.querySelector("html");
+
+if (localStorage.getItem("theme")) {
+  theme.classList.remove("dark")
+  theme.classList.add(localStorage.getItem("theme"));
+} else {
+  localStorage.setItem("theme", "dark");
+}
+
+[...btn].map((el) =>
+  el.addEventListener("click", () => {
+    let mode = localStorage.getItem("theme");
+    if (mode === "dark") {
+      theme.classList.add("light");
+      theme.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      theme.classList.remove("light");
+      theme.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  })
+);
